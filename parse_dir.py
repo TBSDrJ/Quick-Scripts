b4854f16f68b035e28b64cd1e25742a830b499ad
TBSDrJ <marcus.jaiclin@bishops.com>
2020-05-04 09:55:47 -0700

    Find files over 10MB

--- parse_dir.py
+++ parse_dir.py
@@ -6,13 +6,20 @@ dirFile = open(MY_HOME_FOLDER + '/Downloads/dir.txt', 'r')
 
 bigFiles = []
 # For testing, look at only the first chunk of files
-tmpCounter = 0
+# tmpCounter = 0
 for line in dirFile:
-    tmpCounter += 1
-    if tmpCounter > 10:
-        break
+    # tmpCounter += 1
+    # if tmpCounter > 10:
+    #     break
     # To separate out files from other lines
     entry = line.split()
     # If this is true, we probably have a line with a file
     if len(entry) > 8:
-        print(entry)
+        # Field [4] will have the file size
+        # But sometimes maybe not, skip the others
+        # Right now, print lines with files over 10M
+        try:
+            if int(entry[4]) > 10000000:
+                print(entry)
+        except:
+            pass
